556377d5fb18bfbd30984decbf6919335f016b18
unknown <miloszz87@gmail.com>
2020-04-17 20:14:35 -0400

    Created Topics

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,52 @@
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+
+let data = axios.get('https://lambda-times-backend.herokuapp.com/topics')
+.then( function (response) {
+    console.log(response);
+    /*
+    // Save All Topics in variable
+    let topicDiv = createTopic(response.data.topics);
+    console.log(topicDiv);
+
+    // Select Topics Element
+    let topicsDiv = document.getElementsByClassName('topics')[0];
+    
+    // Append Topics Element
+    topicsDiv.appendChild(topicDiv);
+    */
+
+    // Save All Topics in variable
+    let topicArr = response.data.topics;
+    console.log(topicArr);
+   
+    topicArr.forEach(topic => {
+        let newTopic = createTopic(topic)
+        // Select Topics Element
+        let topicsDiv = document.getElementsByClassName('topics')[0];
+       
+        // Append Topics Element
+        topicsDiv.appendChild(newTopic);
+    })
+})
+.catch(function (error) {
+    console.log(error);
+});
+
+
+
+// Topic Component
+const createTopic = (topic) => {
+
+    // Create Element
+    const newTopic = document.createElement('div');
+
+    // Add Style
+    newTopic.classList.add('tab');
+
+    // Add Content
+    newTopic.textContent = topic;
+
+    return newTopic;
+}
